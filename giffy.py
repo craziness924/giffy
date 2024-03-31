f533ba7b5e79c84f5ba350ea1b49a46f0bf501f3
craziness924 <craziness924@example.com>
2024-03-31 13:04:52 -0400

    Changed to os.listdir, requirements.txt added

--- giffy.py
+++ giffy.py
@@ -2,34 +2,32 @@ from PIL import Image
 import argparse
 import os
 
-
-
 def make_gif(input, duration):
-    output = f"{input}\output.gif"
+    output = os.path.join(input, "output.gif")
     
     start_image = None
     images = []
 
-    for i, file in enumerate(os.scandir(input), start=0):
-        file_type = os.path.splitext(file.path)[1]
+    for file in sorted(os.listdir(input)):
+        file_path = os.path.join(input, file)
+        file_type = os.path.splitext(file_path)[1]
         
-        if file_type not in [".png", ".jpeg", ".gif"] or file.path == output:
+        if file_type not in [".png", ".jpg", ".jpeg", ".gif"] or file_path == output:
             continue
 
-        img = Image.open(file.path)
+        img = Image.open(file_path)
 
         images.append(img)
         
-    start_image = images[0]
+    start_image: Image = images[0]
 
     start_image.save(output, save_all=True, append_images=images, duration=float(duration), loop=0)
     
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Giffy", description="Makes gif from list of images")
     parser.add_argument("--input", help="Folder containing images")
-    parser.add_argument("--dur", required=False, help="Time (in milliseconds) to display each frame")
+    parser.add_argument("--dur", required=False, default=400, help="Time (in milliseconds) to display each frame")
 
     args = parser.parse_args()
 
-    make_gif(args.input, args.dur)    
+    make_gif(args.input, args.dur)
